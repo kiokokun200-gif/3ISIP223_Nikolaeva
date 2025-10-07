2f5c21fc3b57c0107938d80b4540061c2c86204e
kiokokun200-gif <kiokokun200@gmail.com>
2025-10-07 13:54:53 +0300

    Добавлен класс курсов

--- 3ISIP223_Nikolaeva/Program.cs
+++ 3ISIP223_Nikolaeva/Program.cs
@@ -21,29 +21,58 @@ namespace _3ISIP223_Nikolaeva
         }
 
 
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"ID: {ID}, ФИО:  {Name}, Дата рождения: {DateOfBirth}, Пол: {Gender}, ");
+        }
+
     }
+
 
     class Student : Person 
     {
         public static int NextID = 1;
 
-        public List<int> Marks {  get; set; }
+        //public List<int> Marks {  get; set; }
         public List<Course> courses { get; set; }
 
 
-        public Student(int ID, string name, DateOnly date, char gender, )   
+        public Student(int ID, string name, DateOnly date, char gender ) : base( name, date, gender )  
         {
-            
+            ID = NextID;
+            NextID++;
         }
 
-        public void PrintInfo()
+        public override void PrintInfo()
         {
+            base.PrintInfo();
+            if (courses != null)
+            {
+                foreach (var course in courses)
+                {
+                    ConsoleWriteLine(course.Name);
 
+                }
+            }
+            else {
+                ConsoleWriteLine("Нет курсов");
+            }
         }
 
         public void PrintCourses()
         {
+            if (courses != null)
+            {
+                foreach (var course in courses)
+                {
+                    ConsoleWriteLine(course.Name);
 
+                }
+            }
+            else
+            {
+                ConsoleWriteLine("Нет курсов");
+            }
         }
 
 
@@ -56,8 +85,23 @@ namespace _3ISIP223_Nikolaeva
         public int Cabinet { get; set; }
         public List<Course> courses { get; set; }
 
-        public void PrintInfo()
+        public override void PrintInfo()
         {
+            base.PrintInfo();
+            Console.Write($"Кабинет: {Cabinet}, ");
+            Console.Write("Курсы: ");
+            if (courses != null)
+            {
+                foreach (var course in courses)
+                {
+                    Console.Write($"{ course.Name} ");
+                }
+            }
+
+            else
+            {
+                Console.Write("Нет курсов");
+            }
 
         }
 
